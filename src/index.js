79e5cc6724c04968daed62242679996ef1000bc0
Nikita <nikitakharya09@gmail.com>
2023-05-01 15:17:44 -0400

    Public path edit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { HashRouter } from "react-router-dom";
 import App from "./App";
 import "./App.scss";
 import { ParallaxProvider } from "react-scroll-parallax";
@@ -8,10 +7,8 @@ import { ParallaxProvider } from "react-scroll-parallax";
 const el = document.getElementById("app");
 
 ReactDOM.render(
-  <HashRouter base="/">
     <ParallaxProvider>
       <App />
-    </ParallaxProvider>
-  </HashRouter>,
+    </ParallaxProvider>,
   el
 );
